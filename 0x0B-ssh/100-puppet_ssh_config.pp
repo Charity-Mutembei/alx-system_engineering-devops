c312c787349c4011f8bfe1618f95c1d2414525ec
Charity Mutembei <charity.softwaredev@gmail.com>
2023-08-28 00:31:45 +0300

    2 file(s)  edited: README.md 100-puppet_ssh_config.pp Pushed at 2023...

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #configure private key
 #refuse password for authentication
-file { '~/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure => present,
   content => "# Managed by Puppet\n\nHost *\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no\n",
 }
