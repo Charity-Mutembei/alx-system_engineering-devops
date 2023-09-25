898863b885c826c2a7f43f06ce8efed36f772c70
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-26 01:29:32 +0300

    2 file(s)  edited: README.md 0-gather_data_from_an_API.py created:  ...

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,36 +10,28 @@ import requests
 import sys
 
 
-def get_employee_info(employee_id):
-    """
-    the function to the sorting and output
-    """
+if len(sys.argv) != 2:
+    print("Usage: python script.py <employee_id>")
+    sys.exit(1)
 
-    base_url = 'https://jsonplaceholder.typicode.com/users'
-    id_url = f'{base_url}/{employee_id}/todos'
-    name_url = f'{base_url}/{employee_id}'
+employee_id = sys.argv[1]
 
-    session = requests.Session()
-    employee_data = session.get(id_url).json()
-    employee_name = session.get(name_url).json()['name']
+base_url = 'https://jsonplaceholder.typicode.com/users'
+id_url = f'{base_url}/{employee_id}/todos'
+name_url = f'{base_url}/{employee_id}'
 
-    total_tasks = sum(1 for task in employee_data if task['completed'])
+session = requests.Session()
+employee_data = session.get(id_url).json()
+employee_name = session.get(name_url).json()['name']
 
-    completed_tasks_message = (
-        f"Employee {employee_name} is done with tasks "
-        f"({total_tasks}/{len(employee_data)}):"
-    )
-    print(completed_tasks_message)
+total_tasks = sum(1 for task in employee_data if task['completed'])
 
-    for task in employee_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
+completed_tasks_message = (
+    f"Employee {employee_name} is done with tasks "
+    f"({total_tasks}/{len(employee_data)}):"
+)
+print(completed_tasks_message)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    get_employee_info(employee_id)
+for task in employee_data:
+    if task['completed']:
+        print(f"\t{task['title']}")
