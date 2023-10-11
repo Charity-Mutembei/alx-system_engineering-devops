1e3dd62d4ed40a48e732ba08b68c33a5ce70d19a
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-11 23:18:35 +0300

    2 file(s)  edited: README.md 0-strace_is_your_friend.pp created:  Pu...

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 # create the missing configuration file
 
 exec {'Fixer':
-  command => "strace -f -e trace=file -o /path/to/output.log /usr/sbin/apache2",
+  command => 'strace -f -e trace=file -o /path/to/output.log /usr/sbin/apache2',
   path    => '/usr/bin/:/usr/local/bin/:/bin/'
 }
