b56f382dedfe2ceab6c5d30d56ab2c19346c0e60
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-11 23:19:41 +0300

    2 file(s)  edited: README.md 0-strace_is_your_friend.pp created:  Pu...

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,11 @@
 # Define a Puppet resource to
 # create the missing configuration file
 
-exec {'Fixer':
-  command => 'strace -f -e trace=file -o /path/to/output.log /usr/sbin/apache2',
-  path    => '/usr/bin/:/usr/local/bin/:/bin/'
+exec { 'diagnose_apache_issue':
+command     => 'strace -f -p <apache_process_id> 2>&1',
+path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+refreshonly => true,
+logoutput   => true,
+unless      => 'grep -q "Specific Error Condition" /var/log/strace_output.log', # Replace with a unique error condition
+notify      => Exec['fix_apache_issue'],
 }
