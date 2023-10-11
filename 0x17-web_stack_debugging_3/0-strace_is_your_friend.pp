80aaa7d0bd5733bc0e322df17573849903dabc13
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-11 23:26:58 +0300

    2 file(s)  edited: README.md 0-strace_is_your_friend.pp created:  Pu...

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,5 +3,5 @@
 
 exec { 'diagnose_apache_issue':
 command => 'strace -f -p <apache_process_id> 2>&1',
-path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+path    => ['/usr/bin/', '/usr/local/bin/', '/bin/'],
 }
