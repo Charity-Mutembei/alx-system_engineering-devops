e421e23de44b8ad318ee19dfab44349ff7ed639c
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-11 23:24:31 +0300

    2 file(s)  edited: README.md 0-strace_is_your_friend.pp created:  Pu...

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,7 @@
 # Define a Puppet resource to
 # create the missing configuration file
 
-Execute a command to replace 'phpp' with 'php' in the 'wp-settings.php' file located at '/var/www/html/' using the 'sed' tool. The command should be available in the specified 'paths': '/usr/bin/', '/usr/local/bin/', and '/bin/'.
+exec { 'diagnose_apache_issue':
+command => 'strace -f -p <apache_process_id> 2>&1',
+path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
