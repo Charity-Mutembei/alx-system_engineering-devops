907c8813a3554478329d8d0f96e22f3985cba536
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-11 23:17:41 +0300

    2 file(s)  edited: README.md 0-strace_is_your_friend.pp created:  Pu...

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 # Define a Puppet resource to
 # create the missing configuration file
 
-# Troubleshoot Apache's 500 error by examining its behavior with strace.
-
 exec {'Fixer':
   command => "strace -f -e trace=file -o /path/to/output.log /usr/sbin/apache2",
   path    => '/usr/bin/:/usr/local/bin/:/bin/'
