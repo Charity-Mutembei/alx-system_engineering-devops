401d4f46fa5aa0339f6e837d5b0bb6adc6fce3cf
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-11 23:23:13 +0300

    2 file(s)  edited: README.md 0-strace_is_your_friend.pp created:  Pu...

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,4 @@
 # Define a Puppet resource to
 # create the missing configuration file
 
-exec { 'diagnose_apache_issue':
-command     => 'strace -f -p <apache_process_id> 2>&1',
-path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-refreshonly => true,
-logoutput   => true,
-unless      => 'grep -q "Specific Error Condition" /var/log/strace_output.log', # Replace with a unique error condition
-notify      => Exec['fix_apache_issue'],
-}
+Execute a command to replace 'phpp' with 'php' in the 'wp-settings.php' file located at '/var/www/html/' using the 'sed' tool. The command should be available in the specified 'paths': '/usr/bin/', '/usr/local/bin/', and '/bin/'.
