341d88f745b8ead16f240262c9b14612801c66c5
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-19 14:47:39 +0300

    4 file(s)  edited: README.md 2-setup_datadog 2-app_server-nginx_conf...

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # Changes file limit details
 
 exec { 'Increase nginx limit':
-  command => "sed -i 's/15/10000/' /etc/default/nginx",
-  path    => '/usr/bin:/usr/local/bin:/bin',
+  command   => "sed -i 's/15/10000/' /etc/default/nginx",
+  path      => '/usr/bin:/usr/local/bin:/bin',
   subscribe => Service['nginx'],
 }
 
