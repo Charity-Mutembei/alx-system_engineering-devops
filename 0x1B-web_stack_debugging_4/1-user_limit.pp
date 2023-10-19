c04ef83ea8a2c60716f47ecb1ad6a4615f01b531
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-19 15:00:03 +0300

    4 file(s)  edited: README.md 2-setup_datadog 2-app_server-nginx_conf...

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,8 @@
 # Define the user whose file limit needs to be adjusted
 $target_user = 'holberton'
 
-include stdlib
-
-file_line { 'remove_contents':
-  ensure => absent,
-  line   => '#File erased',
-  match  => '^.*',
-  path   => '/etc/security/limits.conf',
+file { 'loginFile':
+    ensure  => present,
+    path    => '/etc/security/limits.conf',
+    content => '#File erased'
 }
-
