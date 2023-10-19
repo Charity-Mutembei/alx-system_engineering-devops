0cd94da96981d887e73fd549ee94a4a5ac2ced15
Charity Mutembei <charity.softwaredev@gmail.com>
2023-10-19 14:59:25 +0300

    4 file(s)  edited: README.md 2-setup_datadog 2-app_server-nginx_conf...

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,29 +1,12 @@
 # Define the user whose file limit needs to be adjusted
 $target_user = 'holberton'
 
-# Define the desired file descriptor limit
-$file_limit = 10000
+include stdlib
 
-# Create the user
-user { $target_user:
-  ensure     => 'present',
-  managehome => false,
-  shell      => '/bin/bash',
-  home       => "/home/${target_user}",
-  uid        => undef,
-  gid        => undef,
-  groups     => [],
-  allowdupe  => false,
-  system     => false,
-  password   => '*',
-  expiry     => 'absent',
+file_line { 'remove_contents':
+  ensure => absent,
+  line   => '#File erased',
+  match  => '^.*',
+  path   => '/etc/security/limits.conf',
 }
 
-# Set ulimit for the user
-exec { "Set ulimit for ${target_user}":
-  command   => "ulimit -n ${file_limit}",
-  path      => '/bin:/usr/bin',
-  user      => $target_user,
-  logoutput => true,
-  require   => User[$target_user],
-}
